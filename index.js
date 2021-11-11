8a5229fb0c904528d47f1ff7625495a937d44344
dabit3 <dabit3@gmail.com>
2021-11-11 16:05:48 +0100

    updated snapshot code

--- index.js
+++ index.js
@@ -7,15 +7,35 @@ const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/' + INFURA_KEY))
 const erc721 = require("@0xcert/ethereum-erc721/build/erc721.json").ERC721
 const contract = new web3.eth.Contract(erc721.abi, CONTRACT_ACCOUNT)
-// var idToOwner = {}
-let addresses = []
 
-contract.getPastEvents('Transfer', {fromBlock: CONTRACT_START, toBlock: CONTRACT_START+60000}).then(events => {
+let addresses = []
+let count = 0
+let idToNumber = {}
+
+contract.getPastEvents('Transfer', { fromBlock: CONTRACT_START }).then(events => {
   events.forEach(event => {
-    // idToOwner[event.returnValues._tokenId] = event.returnValues._to
-    addresses.push(event.returnValues._to)
+    /* if the user does not yet have one token, add one */
+    if (!idToNumber[event.returnValues._to]) {
+      idToNumber[event.returnValues._to] = 1
+    } else {
+      /* if they already have one, add another one */
+      idToNumber[event.returnValues._to] = idToNumber[event.returnValues._to] + 1
+    }
+    if (idToNumber[event.returnValues._from]) {
+      /* if the user is sending a token to someone else, remove the token from their count */
+      idToNumber[event.returnValues._from] = idToNumber[event.returnValues._from] - 1
+    }
   });
-  console.log(addresses);
-  addresses = [...new Set(addresses)]
-  fs.writeFileSync('./snapshot.json', JSON.stringify(addresses))
+
+  const filteredArr = []
+
+  Object.entries(idToNumber).map(item => {
+    if (item[1] !== Number()) {
+      filteredArr.push(item[0])
+    }
+  })
+
+  console.log('filteredArr: ', filteredArr.length)
+
+  fs.writeFileSync('./snapshot.json', JSON.stringify(filteredArr))
 });
